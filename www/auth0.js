55931eca980f93b2bbf3896dae8f58b7e5d58015
siacomuzzi <sebastian.iacomuzzi@gmail.com>
2013-11-26 20:28:55 -0300

    testing with auth0.js

--- www/auth0.js
+++ www/auth0.js
@@ -1,18 +1,27 @@
-var config = {
-  authorizeUrl:           'https://{namespace}/authorize',
-  loginWidgetUrl:         'https://{namespace}/login/',
-  resourceOwnerEndpoint:  'https://{namespace}/oauth/ro',
-  userInfoEndpoint:       'https://{namespace}/userinfo?access_token=',
-  defaultCallback:        'https://{namespace}/mobile'
+function Auth0Client(domain, clientId, callbackURL) {
+
+  if (!(this instanceof Auth0Client)) {
+    return new Auth0Client(domain, clientId, callbackURL);
+  }
+
+  this.auth0 = new Auth0({
+    domain:       domain,
+    clientID:     clientId,
+    callbackURL:  callbackURL
+  });
+}
+
+Auth0Client.prototype.login = function (options, callback) {
+
+  if (typeof options === 'function') {
+    callback = options;
+    options = {};
+  }
+
+  if (!options) options = {};
+  if (!callback) callback = function () { };
+
+  this.auth0.login(options, callback);
 };
 
-module.exports = {
-  initialize: function(domain, clientId, clientSecret) {
-    config.domain = domain;
-    config.clientId = clientId;
-    config.clientSecret = clientSecret;
-  },
-  login: function(options, callback) {
-    // TODO
-  }
-};
+module.exports = Auth0Client;
